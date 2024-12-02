5fd0b216ab476fbe92b1c61d6a60383d0eab7f50
ADH-17 <ayden.humphries17@gmail.com>
2024-12-02 12:37:35 -0800

    changes to the addition function

--- Addition.c
+++ Addition.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
 
-int array_Add(int op1[], int op2[], int length);
+void array_Add(int op1[], int op2[], int result[], int length);
 int array1[] = {1,2,3};
 int array2[] = {1,2,3};
-int x;
+int result[] = {0,0,0};
+int length = 3;
 
 int main(){
-    printf("Adding array 1,2,3 with 1,2,3 should return 2,4,6: %d\n", array_Add(array1,array2, 3)); 
-    printf("\n");
-    printf("\n"); 
+    
+    array_Add(array1, array2, result, length);
+
+    printf("Adding array {1,2,3} with array {1,2,3} should return 2,4,6 \n");
+    for (int i = 0; i < length; ){
+        printf("%d, ", result[i]);
+    }
 return 0;
 }
--- addition.s
+++ addition.s
@@ -3,29 +3,29 @@
 
 array_Add:
 
-                        # rdi is the first array of integers
-                        # rsi is the second array of integers
-                        # rdx is the result
-                        # rcx is the length of the arrays
-                        # r12 is the index
+                            # rdi is the first array of integers
+                            # rsi is the second array of integers
+                            # rdx is the result
+                            # rcx is the length of the arrays
+                            # r12 is the index
 
-cmpq $0, %rcx           # length - 0
-jz _done                # jump to done if length is 0
+cmpq $0, %rcx               # length - 0
+jz _done                    # jump to done if length is 0
 
-movl $0, %eax           # Set default value as 0 
-xorq %r12, %r12         # make sure the index is set to 0 
+movl $0, %eax               # Set default value as 0 
+xorq %r12, %r12             # make sure the index is set to 0 
 
 _add: 
-cmpq %rcx, %r12         # size - index
-jle _done               # if size is = index or less, be done
+cmpq %rcx, %r12             # size - index
+jge _done                   # if size is = index or less, be done
 
-movq (%rdi, %r12), %r13 # move the value of the index of the first array into %r13
-movq (%rsi, %r12), %r14 # move the value of the second array at index to %r14
+movq (%rdi, %r12, 8), %r13  # move the value of the index of the first array into %r13
+movq (%rsi, %r12, 8), %r14  # move the value of the second array at index to %r14
 
-addq %r13, %r14         # arr1[i] + arr2[i] stored in %r13
-movq %r13, (%rax, %r12) # move the value into %rax at the current index
-incq %r12               # incriment the index by one
+addq %r14 , %r13            # arr1[i] + arr2[i] stored in %r13
+movq %r13, (%rax, %r12, 8)  # move the value into %rax at the current index
+incq %r12                   # incriment the index by one
 jmp _add
 
 _done:
-    ret                 # returns %rax
+    ret                     # returns %rax
